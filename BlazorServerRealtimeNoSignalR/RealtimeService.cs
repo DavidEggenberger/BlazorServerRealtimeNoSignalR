a0942e0404f6245639cc8efd79faa62f34b6e936
DannyAinge44 <72417712+DannyAinge44@users.noreply.github.com>
2020-10-06 00:13:37 +0200

    Add files via upload

---
+++ BlazorServerRealtimeNoSignalR/RealtimeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerRealtimeNoSignalR
+{
+    public class RealtimeService
+    {
+        private int _a;
+        public int a
+        {
+            get { return _a; }
+            set 
+            { 
+                _a = value;
+                UpdateEvent?.Invoke();
+            }
+        }
+        private int _b;
+
+        public int b
+        {
+            get { return _b; }
+            set 
+            { 
+                _b = value;
+                UpdateEvent?.Invoke();
+            }
+        }
+        public event Func<Task> UpdateEvent;
+    }
+}
